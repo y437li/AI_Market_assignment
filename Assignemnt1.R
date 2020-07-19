7ecfb6790eff63f52870e8187cdea80617865e21
y437li <li.mac.kaka@gmail.com>
2020-07-19 10:21:18 -0400

    update

--- Assignemnt1.R
+++ Assignemnt1.R
@@ -1,21 +1,22 @@
 ##Load data from local csv file
 path = '/Users/yangli/OneDrive/MMAI/MMAI831/AIOS1_adv_sales.csv'
+#path = 'C:/Users/y437l/OneDrive/MMAI/MMAI831/AIOS1_adv_sales.csv'
 data <- read.csv(file =path)
 #drop index column
 data <- subset(data, select = -c(X))
 ##No missing data
 #basic descriptive statistics
-summary(data)
-
+summary(data[,c(1:6)])
+data[,1:6]<- scale(data[,1:6])
 #check data suitability
 #generalized pair grapjs to check for bivariate correlations
 library(gpairs)
-gpairs(data)
+gpairs(data[,c(1:6)])
 
 ###seems like there is no correlation relationship between independent variables
 library(corrplot)
-corrplot.mixed(cor(data[,c(1:6)]),upper = "square",bg="lightblue")
-cor(data[,c(1:6)])
+corrplot(cor(data[,c(1:6)]), method = "color", type="full", addgrid.col = "red",
+         addshade = "positive", addCoef.col = "black")
 ##The goal of a sales driver analysis is to discover relationships between
 ##the sale volume with features of the the price and number of stores for the 
 ## product and different type of advertisements.
@@ -49,8 +50,9 @@ anova(model2)
 #R squared
 summary(model2)
 # check heteroskedasticity
-par(mfrow=c(2,2))
+par(mfrow=c(1,1))
 plot(model2)
+plot(model2$residuals)
 ###Breusch-pagen test
 bptest(model2)
 ###Durbin-Watson test serial correlation
@@ -73,8 +75,9 @@ anova(model3)
 #R squared
 summary(model3)
 # check heteroskedasticity
-par(mfrow=c(2,2))
+par(mfrow=c(1,1))
 plot(model3)
+plot(model3$residuals)
 ###Breusch-pagen test
 bptest(model3)
 ###Durbin-Watson test serial correlation
@@ -97,11 +100,12 @@ anova(model4)
 #R squared
 summary(model4)
 # check heteroskedasticity
-par(mfrow=c(2,2))
+par(mfrow=c(1,1))
 plot(model4)
 ###Breusch-pagen test
 bptest(model4)
 ###Durbin-Watson test serial correlation
+plot(model4$residuals)
 dwtest(model4)
 
 #predict test data
@@ -121,8 +125,9 @@ anova(model5)
 #R squared
 summary(model5)
 # check heteroskedasticity
-par(mfrow=c(2,2))
+par(mfrow=c(1,1))
 plot(model5)
+plot(model5$residuals)
 ###Breusch-pagen test heteroskedasticity
 bptest(model5)
 ###Durbin-Watson test serial correlation
@@ -145,8 +150,9 @@ anova(model6)
 #R squared
 summary(model6)
 # check heteroskedasticity
-par(mfrow=c(2,2))
+par(mfrow=c(1,1))
 plot(model6)
+plot(model6$residuals)
 ###Breusch-pagen test heteroskedasticity
 bptest(model6)
 ###Durbin-Watson test serial correlation
@@ -169,8 +175,9 @@ anova(model5_1)
 #R squared
 summary(model5_1)
 # check heteroskedasticity
-par(mfrow=c(2,2))
+par(mfrow=c(1,1))
 plot(model5_1)
+plot(model5_1$residuals)
 ###Breusch-pagen test heteroskedasticity
 bptest(model5_1)
 ###Durbin-Watson test serial correlation
@@ -184,4 +191,55 @@ test_R_squared5_1 = 1 - (RSS5_1/TSS5_1)
 ###########################
 #train R squared:0.9201
 #test R squared:0.92026
+###########################
+##Multiple linear regression with nine factors
+model9<-lm(sales~price+store+billboard+printout+sat+comp
+           +store:billboard+store:printout+billboard:printout,data=train_data)
+##Anova table,F-test
+anova(model9)
+#R squared
+summary(model9)
+# check heteroskedasticity
+par(mfrow=c(1,1))
+plot(model9)
+plot(model9$residuals)
+###Breusch-pagen test heteroskedasticity
+bptest(model9)
+###Durbin-Watson test serial correlation
+dwtest(model9)
+
+#predict test data
+model9_test_result <- predict(model9, newdata=test_data)
+TSS9 <- sum((test_data$sales-mean(test_data$sales))^2)
+RSS9 <- sum((test_data$sales-model9_test_result)^2)
+test_R_squared9 = 1 - (RSS9/TSS9)
+###########################
+#train R squared:0.9259
+#test R squared:0.92839
+###########################
+
+##Multiple linear regression with six factors
+model6_1<-lm(sales~price+store+billboard+sat+comp
+           +store:billboard,data=train_data)
+##Anova table,F-test
+anova(model6_1)
+#R squared
+summary(model6_1)
+# check heteroskedasticity
+par(mfrow=c(1,1))
+plot(model6_1)
+plot(model6_1$residuals)
+###Breusch-pagen test heteroskedasticity
+bptest(model6_1)
+###Durbin-Watson test serial correlation
+dwtest(model6_1)
+
+#predict test data
+model6_1_test_result <- predict(model6_1, newdata=test_data)
+TSS6_1 <- sum((test_data$sales-mean(test_data$sales))^2)
+RSS6_1 <- sum((test_data$sales-model6_1_test_result)^2)
+test_R_squared6_1 = 1 - (RSS6_1/TSS6_1)
+###########################
+#train R squared:0.9257
+#test R squared:0.9284961
 ###########################
